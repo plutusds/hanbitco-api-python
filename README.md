9f8ce01d3c3ca0cdf7060bdd062c8431d6ec2cfb
Kevin Kim <kevink1103@gmail.com>
2021-03-18 16:44:15 +0900

    Author email and README

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 ![Hanbitco](https://dka575ofm4ao0.cloudfront.net/pages-transactional_logos/retina/177237/Hanbitco-Logo.png)
 
+[![made-with-python](https://img.shields.io/badge/Made%20with-Python-1f425f.svg)](https://www.python.org/)
+[![PyPI pyversions](https://img.shields.io/pypi/pyversions/hanbitco-api)](https://pypi.org/project/hanbitco-api/)
+[![MIT LICENSE](https://img.shields.io/pypi/l/hanbitco-api)](https://github.com/plutusds/hanbitco-api/blob/main/LICENSE)  
+[![PyPI version](https://badge.fury.io/py/hanbitco-api.svg)](https://badge.fury.io/py/hanbitco-api)
+
 # Hanbitco API Python
 
 Official API Python Wrapper for [Hanbitco Exchange](https://www.hanbitco.com).
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-version = "1.0.1"
+version = "1.0.2"
 
 with open("README.md", encoding="utf-8") as f:
     long_description = f.read()
@@ -8,25 +8,23 @@ with open("README.md", encoding="utf-8") as f:
 setup(
     name             = "hanbitco-api",
     version          = version,
-    description      = "Python Wrapper for Hanbitco API",
+    description      = "Offical API Python Wrapper for Hanbitco Exchange",
     long_description = long_description,
     long_description_content_type = "text/markdown",
     author           = "Kevin Kim",
-    author_email     = "kevink1103@gmail.com",
+    author_email     = "kevink1103@plutusds.com",
     license          = "MIT",
     url              = "https://github.com/plutusds/hanbitco-api",
     download_url     = "https://github.com/plutusds/hanbitco-api/dist/hanbitco_api-{}-py3-none-any.whl".format(version),
     install_requires = [],
     packages         = find_packages(exclude = []),
     keywords         = ["cryptocurrency", "exchange", "api"],
-    python_requires  = ">=3.3",
+    python_requires  = ">=3.6",
     zip_safe         = False,
     classifiers      = [
-        "Programming Language :: Python :: 3.3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ]
 )
